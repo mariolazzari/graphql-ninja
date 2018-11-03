c1bf941189725818fc2886db7de7ca5bcc78ddeb
mariolazzari <mario.lazzari@gmail.com>
2018-11-03 23:22:24 +0100

    Done lesson 8

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const graohqlHTTP = require("express-graphql");
 
 const app = express();
+app.use("/graphql", graohqlHTTP({}));
 
 app.listen(4000, () => "Server started on port 4000");
